26e4dd551c10752cb9dfbfac104805131ab73871
Mihir Hadavani <128199131+mihirh19@users.noreply.github.com>
2023-04-01 09:10:40 +0530

    ✅ Ready for production

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
-// const mongoURI = "mongodb+srv://mihir:admin@cluster0.csp0gt1.mongodb.net/test"
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully");
